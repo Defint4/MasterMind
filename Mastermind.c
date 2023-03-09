925bb8826cf76c0ec28e69440f5d89e6c3de3be5
Defint4 <guiot.matthieu@live.fr>
2023-03-09 14:12:18 +0100

    Mastermind

--- Mastermind.c
+++ Mastermind.c
@@ -4,7 +4,6 @@
 
 
 char *generate() {
-    srand(time(NULL));
     char tableau[6] = {'P', 'B', 'M', 'Y', 'O', 'G'};
     int secret[4];
     char *code = malloc(sizeof(char) * 4);
@@ -19,14 +18,14 @@ char *generate() {
     for(int j = 0; j < 4; j++){
         code[j] = tableau[secret[j]];
     }
-    // printf("%c %c %c %c\n", code[0], code[1], code[2], code[3]);
+    printf("%c %c %c %c\n", code[0], code[1], code[2], code[3]);
     return code;
 }
 
 char *response() {
     char *response = malloc(sizeof(char) * 4);                   
     printf("\n Entrez les 4 couleur en maj(ex : P B M Y) : ");
-    scanf("%c %c %c %c", &response[0], &response[1], &response[2], &response[3]); 
+    scanf(" %c %c %c %c", &response[0], &response[1], &response[2], &response[3]); 
     printf("-----------\n");                                                                          // on demande à l'utilisateur de rentrer 4 couleurs
     printf("| %c %c %c %c |\n", response[0], response[1], response[2], response[3]);
     return response;
@@ -34,47 +33,41 @@ char *response() {
 
 int verify() {
     char *secret = generate();
+    char result[4];
     for(int round = 0; round < 12; round++) {
         char *responseUser = response();
         int countRed = 0;
         int countWhite = 0;
         for(int i = 0; i < 4; i++) {
-            if (responseUser[i] == secret[i]) {
-                printf("W");                            // on test si ya des bien placé
+            int passed = 0;
+            if (responseUser[i] == secret[i]) {                               // on test si ya des bien placé
                 countWhite++;
+                passed = 1;
             }
-        }
                 if(countWhite == 4 ){
                 printf("\nBravo vous avez gagné");
                 return 1;
                 }
-        for(int b = 0; b < 4; b++) {
             for(int j = 0; j < 4; j++) {
-                if (responseUser[b] == secret[j]) {
-                    printf("R");                    // on test si y'a des mal placé 
-                    countRed++;
+                    if (responseUser[i] == secret[j] && passed == 0) {                           // on test si y'a des mal placé 
+                        countRed++;
+                    }
                 }
             }
+        for(int i = 0; i < countWhite; i++) {
+            printf("W");
         }
-        if(countRed + countWhite == 4) {
-            printf("");
+        for(int i = 0; i < countRed; i++) {
+            printf("R");
         }
-        else if(countRed + countWhite == 3) {
+        for (int i = 0; i < 4 - countRed - countWhite; i++) {
             printf(".");
-        } 
-        else if(countRed + countWhite == 2) {
-            printf("..");                            // si jamais y'a pas de mal placé ni de bien placé
         }
-        else if(countRed + countWhite == 1) {
-            printf("...");
-        }
-        else if(countRed + countWhite == 0) {
-            printf("....");
-        } 
     }
 }
 
 int main() {
+    srand(time(NULL));
     verify();
     return 0;
 }
